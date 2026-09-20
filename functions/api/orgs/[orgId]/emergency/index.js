133b62067b4ec56a2201ddd2e2fe7243c0460509
ash.budding <bjgarr@gmail.com>
2026-09-19 21:59:00 -0700

    Add DPG emergency and destruction endpoints

---
+++ functions/api/orgs/[orgId]/emergency/index.js
@@ -0,0 +1,12 @@
+import { readEmergencyStatus } from '../../../_lib/emergency.js';
+
+export async function onRequestGet(ctx) {
+  const { orgId } = ctx.params;
+  const res = await readEmergencyStatus({ env: ctx.env, request: ctx.request, orgId });
+  if (!res.ok) return res.resp;
+  return new Response(JSON.stringify({
+    status: res.status,
+    orgLockdown: res.orgLockdown,
+    orgProtocol: res.orgProtocol,
+  }), { headers: { 'Content-Type': 'application/json' } });
+}
